214f98d717a492d7d26a7225dc0dfb7482689b92
Virinskaya <e.virinskaya@gmail.com>
2021-10-18 18:27:34 +0300

    Version 5. Added Optional Tasks - 5 task

--- src/main/java/collections/optionalTask/EighthTaskEnglishWords.java
+++ src/main/java/collections/optionalTask/EighthTaskEnglishWords.java
@@ -3,16 +3,23 @@ package collections.optionalTask;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class EighthTaskEnglishWords {
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader
-                        (new FileReader(new Scanner(System.in).nextLine()))) {
+                (new FileReader(new Scanner(System.in).nextLine()))) {
+            Set<String> uniqueWords = new HashSet<>();
+            List<String> strings;
+
+            while (reader.ready()) {
+                strings = Arrays.asList(reader.readLine().toLowerCase().split(" "));
+                uniqueWords.addAll(strings);
+            }
+            uniqueWords.forEach(System.out::println);
 
 
-
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.getStackTrace();
         }
     }
